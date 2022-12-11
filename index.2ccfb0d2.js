9c05f2ddec140c9652be6d8a6b9b6a5a7bb9dccd
IvanovArtemNA <113294828+IvanovArtemNA@users.noreply.github.com>
2022-12-11 10:50:16 +0000

    Deploying to gh-pages from @ IvanovArtemNA/template-yarn@92d0857a9347a1bbf56d22f40656fe06aeed7260 🚀

---
+++ index.2ccfb0d2.js
@@ -0,0 +1,2 @@
+new class{showName(){return this.username}constructor(){this.username="Oleksii"}};console.log("Hello");
+//# sourceMappingURL=index.2ccfb0d2.js.map
---
+++ index.8022cc98.js
@@ -0,0 +1,2 @@
+!function(){"use strict";new class{showName(){return this.username}constructor(){this.username="Oleksii"}};console.log("Hello")}();
+//# sourceMappingURL=index.8022cc98.js.map
